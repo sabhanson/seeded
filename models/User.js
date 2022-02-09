a9a93068b3fa8c5997b1e0975577945309c74811
isaac petersen <isaac.petersen5@gmail.com>
2022-02-09 11:05:41 -0800

    user model update

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt')
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+    }
+
 
 User.init({
     username:{
