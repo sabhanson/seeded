b6f954ee21f0fd9e403af555bd7aed6a28ca0670
isaac petersen <isaac.petersen5@gmail.com>
2022-02-09 15:22:00 -0800

    login/signup/logout routes work

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+
+
 router.get('/signup', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
@@ -9,9 +11,23 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 
-
-
+router.get('/dashboard', (req, res)=>{
+    console.log(req.session)
+    if (!req.session.loggedIn) {
+        res.status(403).json({ msg: "You need to login to access this" });
+      } else {
+        res.render('dashboard')
+      }
+})
 
 module.exports = router;
