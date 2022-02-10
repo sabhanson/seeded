e4726e52024c6b9cb356838d86dea110807f83f4
isaac petersen <isaac.petersen5@gmail.com>
2022-02-10 11:35:55 -0800

    mobile fix

---
+++ public/js/landingPage.js
@@ -0,0 +1,10 @@
+$('#myCarousel').carousel()
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
