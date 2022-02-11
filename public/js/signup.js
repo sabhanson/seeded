2263485466897cad373d5cb0ec3c53310819c66b
isaac petersen <isaac.petersen5@gmail.com>
2022-02-10 18:35:47 -0800

    isaac is fired

--- public/js/signup.js
+++ public/js/signup.js
@@ -5,7 +5,7 @@ const signupForm = async function(event) {
     const passwordEl = document.querySelector('#password-input-signup');
     const emailEl = document.querySelector('#email-input-signup');
 
-    const response = await fetch('/api/user/signup', {
+    const response = await fetch('/api/user', {
         method: 'POST',
         body: JSON.stringify({
             username: usernameEl.value,
@@ -16,7 +16,7 @@ const signupForm = async function(event) {
     });
 
     if (response.ok) {
-        document.location.replace('/dashboard');
+        document.location.replace('/profile');
     } else {
         alert('Check your input fields');
     }
