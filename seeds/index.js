93155b2e6b32ef72b467fbeabb5ed6b8e1bdfc50
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-14 15:09:55 -0800

    fixing issues

--- seeds/index.js
+++ seeds/index.js
@@ -1,35 +1,35 @@
-// const sequelize = require("../config/connection");
-// const { User } = require("../models");
+const sequelize = require("../config/connection");
+const { User } = require("../models");
 
-// const users = [
-//   {
-//     username: "sabthegoodwitch",
-//     password: "password",
-//     email: "sabhanson7@gmail.com",
-//   },
-//   {
-//     username: "plantasia",
-//     password: "password",
-//     email: "isaac.petersen5@gmail.com",
-//   },
-//   {
-//     username: "mossbb",
-//     password: "password",
-//     email: "425megs@gmail.com",
-//   },
-//   {
-//     username: "kony2012",
-//     password: "password",
-//     email: "andrewtranmsw@gmail.com",
-//   },
-// ];
+const users = [
+  {
+    username: "sabthegoodwitch",
+    password: "password",
+    email: "sabhanson7@gmail.com",
+  },
+  {
+    username: "plantasia",
+    password: "password",
+    email: "isaac.petersen5@gmail.com",
+  },
+  {
+    username: "mossbb",
+    password: "password",
+    email: "425megs@gmail.com",
+  },
+  {
+    username: "kony2012",
+    password: "password",
+    email: "andrewtranmsw@gmail.com",
+  },
+];
 
 
-// const seed = async () => {
-//   await sequelize.sync({ force: false });
-//   await User.bulkCreate(users, { individualHooks: true });
-//   console.log("all seeded!");
-//   process.exit(0);
-// };
+const seed = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(users, { individualHooks: true });
+  console.log("all seeded!");
+  process.exit(0);
+};
 
-// seed();
+seed();
